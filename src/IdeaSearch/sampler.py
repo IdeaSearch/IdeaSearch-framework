f4cf56ffcdd633791f4dbf373e2d1efef715a1e4
parkcai <162147799+parkcai@users.noreply.github.com>
2025-05-07 21:19:00 +0800

    improve prompt

--- src/IdeaSearch/sampler.py
+++ src/IdeaSearch/sampler.py
@@ -70,9 +70,9 @@ class Sampler:
                 examples_section += f"[第 {index + 1} 个例子]\n"
                 examples_section += f"内容：\n"
                 examples_section += f'{idea}\n'
-                examples_section += f"得分：{score:.2f}\n"
+                examples_section += f"评分：{score:.2f}\n"
                 if info is not None:
-                    examples_section += f"说明：{info}\n"
+                    examples_section += f"评语：{info}\n"
                 if similar_num is not None:
                     examples_section += (
                         f"重复情况说明：目前数据库里有{similar_num}个例子和这个例子相似\n"
